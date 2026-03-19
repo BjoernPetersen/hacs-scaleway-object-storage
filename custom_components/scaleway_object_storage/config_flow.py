ab09faa8c4bc6935c42857e9c246e8a333daec5e
Björn Petersen <git@bjoernpetersen.net>
2026-03-20 00:33:35 +0100

    fix: await coro

--- custom_components/scaleway_object_storage/config_flow.py
+++ custom_components/scaleway_object_storage/config_flow.py
@@ -74,7 +74,7 @@ class ScalewayConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 }
             )
 
-            if self._test_connection(errors=errors, config=user_input):
+            if await self._test_connection(errors=errors, config=user_input):
                 return self.async_create_entry(
                     title=f"Scaleway - {user_input[CONF_BUCKET]}",
                     data=user_input,
